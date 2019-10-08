2f5cf134a76c698945afb2d87bb00cb0fbe46ca0
zhanlele <leylomai@foxmail.com>
2019-10-08 22:47:29 +0800

    Initial commit

---
+++ src/main/java/cn/iot/study/FirstServlet.java
@@ -0,0 +1,31 @@
+package cn.iot.study;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/hello.view")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1> Hello! " + name + " !</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+        out.close();
+    }
+}
