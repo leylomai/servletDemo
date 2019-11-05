4fd9b8bbb34034227eee3cc3f8bd80ce9b35970d
zhanlele <leylomai@foxmail.com>
2019-11-05 18:29:39 +0800

    1105

--- src/main/java/cn/iot/study/FirstServlet.java
+++ src/main/java/cn/iot/study/FirstServlet.java
@@ -1,11 +1,14 @@
 package cn.iot.study;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -31,5 +34,46 @@ public class FirstServlet extends HttpServlet {
         out.println("</html>");
 
         out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String body = readBody(req);
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println(body);
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    private String readBody(HttpServletRequest req) throws IOException {
+        BufferedReader reader = req.getReader();
+        String content = null;
+        StringBuilder strBuilder = new StringBuilder();
+        while (null != (content = reader.readLine())) {
+            strBuilder.append(content);
+        }
+        return strBuilder.toString();
+    }
+
+    private byte[] readBody_1(HttpServletRequest req) throws IOException {
+        int formDataLength = req.getContentLength();
+
+        ServletInputStream sis = req.getInputStream();
+        DataInputStream dataInputStream = new DataInputStream(sis);
+
+        byte[] bodys = new byte[formDataLength];
+
+        int indexBytes = 0;
+        while (indexBytes < formDataLength) {
+            int byteCount = dataInputStream.read(bodys, indexBytes, formDataLength);
+            indexBytes += byteCount;
+        }
+        // 75页
+        return bodys;
     }
 }
