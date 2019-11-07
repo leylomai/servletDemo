87aa1f4e7ccc67877b27ad2fb45972cdd7cc1a0d
zhanlele <leylomai@foxmail.com>
2019-11-07 18:14:25 +0800

    1107

---
+++ src/main/java/cn/iot/study/OtherServlet.java
@@ -0,0 +1,21 @@
+package cn.iot.study;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/other.view")
+public class OtherServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("Other do one, " + String.valueOf(name) + "...");
+        //printWriter.flush();
+        //printWriter.close();    //被include，这里不能close stream;
+    }
+}
---
+++ src/main/java/cn/iot/study/Some.java
@@ -0,0 +1,26 @@
+package cn.iot.study;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/some.view")
+public class Some extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("Some do one...");
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/other.view");
+        dispatcher.include(req, resp);
+
+        printWriter.println("Some do two...");
+        printWriter.flush();
+        printWriter.close();
+    }
+}
