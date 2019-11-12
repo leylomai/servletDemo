ba6b65fa5ab63dd8ad1676b58e7fa90c1f4712e4
zhanlele <leylomai@foxmail.com>
2019-11-12 18:00:03 +0800

    1112

---
+++ src/main/java/cn/iot/study/HelloModel.java
@@ -0,0 +1,19 @@
+package cn.iot.study;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloModel {
+    private Map<String, String> messages = new HashMap<>();
+
+    public HelloModel() {
+        messages.put("caterpillar", "Hello");
+        messages.put("Justin", "Welcome");
+        messages.put("momor", "Hi");
+    }
+
+    public String doHello(String user) {
+        String message = messages.get(user);
+        return message + ',' + user + "!";
+    }
+}
